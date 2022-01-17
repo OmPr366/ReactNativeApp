91af3b1ea4ed68c9e29032587b949a13519d1576
Om Prakash <71120474+OmPr366@users.noreply.github.com>
2022-01-17 19:35:49 +0530

    Color Added in Font

--- App.js
+++ App.js
@@ -4,18 +4,37 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This is Om Prakash!</Text>
-      <Text>I'm Opening my First App</Text>
-      <StatusBar style="auto" />
+      <View style={styles.container}>
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigBlue}>just bigBlue</Text>
+        <Text style={[styles.bigBlue, styles.red]}>bigBlue, then red</Text>
+        <Text style={[styles.red, styles.bigBlue]}>red, then bigBlue</Text>
+      </View>
     </View>
   );
 }
 
+// const styles = StyleSheet.create({
+//   container: {
+    // flex: 1,
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+//   },
+// });
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
   },
 });
