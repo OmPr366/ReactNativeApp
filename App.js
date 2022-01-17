2c369d1b1d84ce2fddb0260c4b3dec25ff4d2f19
Om Prakash <71120474+OmPr366@users.noreply.github.com>
2022-01-17 19:24:13 +0530

    First React Native App

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This is Om Prakash!</Text>
+      <Text>I'm Opening my First App</Text>
       <StatusBar style="auto" />
     </View>
   );
