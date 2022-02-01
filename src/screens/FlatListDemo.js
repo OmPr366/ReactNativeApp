9993f1e8920e02359956c9b03cfc194e49b63af1
Om Prakash <71120474+OmPr366@users.noreply.github.com>
2022-02-02 01:11:07 +0530

    Flat List Added

--- src/screens/FlatListDemo.js
+++ src/screens/FlatListDemo.js
@@ -1,31 +1,48 @@
-import React from 'react';
-import {View, StyleSheet, FlatList} from 'react-native';
+import React from "react";
+import { View,Text, StyleSheet, FlatList } from "react-native";
 
 const Flatlistdemo = () => {
-    const names = [
-        {
-            name:"Prakash"
-        },
-        {
-            name:"Prakash2"
-        },
-        {
-            name:"Prakash3"
-        },
-        {
-            name:"Prakash4"
-        },
-        {
-            name:"Prakash5"
-        },
-    ]
-    return (
-        <View>
-            <FlatList data={names} renderItem={(element)=> {return element.item.name} } />
-        </View>
-    );
-}
+  const names = [
+    {
+      name: "Prakash",
+    },
+    {
+      name: "Prakash2",
+    },
+    {
+      name: "Prakash3",
+    },
+    {
+      name: "Prakash4",
+    },
+    {
+      name: "Prakash5",
+    },
+  ];
+  return (
+    <View>
+      <FlatList
+        data={names}
+        // horizontal
+        // numColumns={2}
+        
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item}) => {
+          return( <Text style={styles.textSyle} >{item.name}</Text>) 
+        }}
+      />
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  textSyle: {
+    fontSize: 40,
+    padding: 40,
+    backgroundColor: "red",
+    margin: 20,
+    color: "white",
+  },
+});
 
 export default Flatlistdemo;
